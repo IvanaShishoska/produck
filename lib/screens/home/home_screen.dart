0af492eae2bbd5edb70df5b1d983645b894c10f6
IvanaShishoska <63503356+IvanaShishoska@users.noreply.github.com>
2022-03-31 16:48:50 +0200

    Add files via upload
    added carousel

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,26 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:mis_homework/models/category_model.dart';
 import '../../widgets/custom_appbar.dart';
 import '../../widgets/custom_navbar.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-class HomeScreen extends StatelessWidget{
+import '../../widgets/hero_carousel_card.dart';
 
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-  static const String routeName = '/';
+  static const String routeName = '/home';
 
-  static Route route(){
+  static Route route() {
     return MaterialPageRoute(
-    settings: const RouteSettings(name: routeName),
-    builder: (_) => const HomeScreen()
-    );
+        settings: const RouteSettings(name: routeName),
+        builder: (_) => const HomeScreen());
   }
 
   @override
   Widget build(BuildContext context) {
-
-    return const Scaffold(
+    return Scaffold(
       appBar: CustomAppBar(title: 'ProDuck'),
       bottomNavigationBar: CustomNavBar(),
+      body: Container(
+          child: CarouselSlider(
+            options: CarouselOptions(
+              aspectRatio: 2.0,
+              enlargeCenterPage: true,
+              enableInfiniteScroll: false,
+            ),
+            items:Category.categories.map((category) => HeroCarouselCard(category: category)).toList(),
+          )),
     );
   }
 }
+
+
