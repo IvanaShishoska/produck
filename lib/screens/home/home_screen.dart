e23787e3e17b0c3662c83944df7d4e1c1f5cf13e
IvanaShishoska <63503356+IvanaShishoska@users.noreply.github.com>
2022-03-31 21:31:48 +0200

    Add files via upload
    Added categories tiles

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:mis_homework/models/category_model.dart';
+import 'package:mis_homework/models/models.dart';
 import '../../widgets/custom_appbar.dart';
 import '../../widgets/custom_navbar.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-
 import '../../widgets/hero_carousel_card.dart';
+import '../../widgets/product_card.dart';
+import '../../widgets/section_title.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -22,17 +23,25 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: CustomAppBar(title: 'ProDuck'),
       bottomNavigationBar: CustomNavBar(),
-      body: Container(
-          child: CarouselSlider(
+      body: Column(
+        children: [
+          Text("POPULAR", style: Theme.of(context).textTheme.headline3),
+          Container(
+              child: CarouselSlider(
             options: CarouselOptions(
               aspectRatio: 2.0,
               enlargeCenterPage: true,
               enableInfiniteScroll: false,
             ),
-            items:Category.categories.map((category) => HeroCarouselCard(category: category)).toList(),
+            items: Product.products
+                .map((product) => HeroCarouselCard( product:product))
+                .toList(),
           )),
+          SectionTitle(title: "ALL CATEGORIES"),
+    //product card
+          ProductCard(product: Product.products[1]),
+        ],
+      ),
     );
   }
 }
-
-
